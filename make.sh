417511be2c7309e743d9ef6f9dcc3d7df85174ad
MyPetIsBear <alexandr.ledovskiy@gmail.com>
2014-01-11 03:08:15 +0400

    first commit

---
+++ make.sh
@@ -0,0 +1,44 @@
+
+# Pdflatex
+PDFLATEX='pdflatex -interaction=nonstopmode -shell-escape -file-line-error'
+GREP='grep ".*:[0-9]*:.*"' # показывает на выходе только ошибки
+
+# Файлы/Папки
+PDF='thesis.pdf'
+TEX='tex'
+INC='inc'
+GOST='gost'
+MAINTEX='thesis'
+
+# Копирование файлов из папки gost
+cd $GOST
+cp -r * ../$TEX
+cd ..
+
+# Конвертация eps
+find -E $INC/ -type f -name "*.eps" -exec epstopdf {} ";" ;
+find -E $INC -type f -name "*.eps" -exec rm -f {} \;
+
+# Копирование файлов из папки img
+cd $INC
+if [[ $(ls) ]]; then
+    cp -r * ../$TEX
+fi
+cd ..
+
+# Сборка latex
+cd tex
+$PDFLATEX $MAINTEX | $GREP
+BIBOUTPUT=$(bibtex $MAINTEX)
+# Показывать output bibtex'a только в случае ошибок
+if [[ "$BIBOUTPUT" =~ "error" ]]; then
+    echo "$BIBOUTPUT"
+fi
+$PDFLATEX $MAINTEX | $GREP
+cp $PDF ..
+cd ..
+
+# Clear
+find -E $TEX/ -type f ! -regex ".*\.(tex|bib|gitignore)" -exec rm -f {} \; ;
+# find -E $TEX/ -type d -exec rm -rf {} \;
+
